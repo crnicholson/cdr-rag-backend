2f323fbf54ca08763ae6f6c5479419d476cd0e2c
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-18 14:52:21 -0400

    Add POST

--- main.py
+++ main.py
@@ -11,5 +11,13 @@ def return_home():
     return jsonify({"time": str(datetime.now())})
 
 
+@app.route("/api/chat", methods=["POST"])
+def send_message():
+    data = request.json
+    message = data.get("message")
+    print(message)
+    return 0
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=7359)
