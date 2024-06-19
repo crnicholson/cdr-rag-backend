fd878f2b096f9aa29583fff3da56abdfd30e1a2c
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-19 13:53:09 -0400

    Cleaning up old code

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from datetime import datetime
-
-# import fitz  # PyMuPDF
-# import textwrap
 from openai import OpenAI
 
 
@@ -26,14 +23,6 @@ def send_message():
     message = data.get("message")
     print(f"Message received: {message}")
     print(f"Current key: {key}")
-    # client = OpenAI(api_key=key)
-    # try:
-    #     response = client.Completion.create(
-    #         engine="text-davinci-003", prompt=message, max_tokens=150
-    #     )
-    #     return jsonify({"message": response.choices[0].text.strip()}), 200
-    # except Exception as e:
-    #     return jsonify({"error": str(e)}), 500
     return jsonify({"message": "Hello, World!"}), 200
 
 
