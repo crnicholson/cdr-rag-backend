38d47d391be4304246f2634af5fa9457a8ba9620
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-18 22:20:34 -0400

    Integrate some ChatGPT

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from datetime import datetime
+import pymupdf
+import textwrap
+from openai import OpenAI
+
+API_KEY = ""
+
+client = OpenAI(api_key=API_KEY)
 
 app = Flask(__name__)
 CORS(app)
 
+key = ""
 
-# @app.route("/api/time", methods=["GET"])
-# def return_home():
-#     return jsonify({"time": str(datetime.now())})
-
-
-@app.route("/api/chat", methods=["POST"])
+@app.route("/api/send-message", methods=["POST"])
 def send_message():
     data = request.json
     message = data.get("message")
     print(message)
-    return jsonify({"message": message})
+    return jsonify({"message": "Testing testing"})
+
+
+@app.route("/api/send-key", methods=["POST"])
+def get_key():
+    data = request.json
+    key = data.get("key")
+    print(key)
+    return ""
 
 
 if __name__ == "__main__":
